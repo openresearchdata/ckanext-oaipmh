153ab1f7326ec21e8e0f1ef808ddf21662e7651e
Stefan Oderbolz <stefan.oderbolz@liip.ch>
2014-12-05 11:00:37 +0100

    Fix linting errors

--- ckanext/oaipmh/command.py
+++ ckanext/oaipmh/command.py
@@ -1,7 +1,5 @@
-from ckan import model
-from ckan.logic import get_action, ValidationError
-
 from ckanext.harvest.commands.harvester import Harvester
+
 
 class OaipmhHarvesterCommand(Harvester):
     """
